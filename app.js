adb3a91528e5f64579db1633a862b9fc3bc3b63d
DSDouglas <55850979+DSDouglas@users.noreply.github.com>
2020-05-20 21:30:39 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,141 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    //start the game
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+        });
+    };
+
+    //play match
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        const hands = document.querySelectorAll('.hands img');
+        const reset = document.querySelector('.reset button');
+
+        hands.forEach(hand => {
+            hand.addEventListener('animationend', function () {
+                this.style.animation = '';
+            });
+        });
+        //computer options
+        reset.addEventListener('click', function(){
+            pScore = 0;
+            cScore = 0;
+            updateScore();
+
+        });
+
+
+
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach(options => {
+            options.addEventListener('click', function () {
+                //computer choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber]
+                //here is where we call compare hands
+
+                setTimeout(()=>{
+                    compareHands(this.textContent, computerChoice);
+
+                    //Update Images
+                    playerHand.src = `./assets/${this.textContent}.png`;
+                    computerHand.src = `./assets/${computerChoice}.png`;
+                }, 2000);
+
+
+
+                //Animation
+
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease";
+
+
+            });
+        });
+    };
+    const updateScore = () => {
+        const playerScore = document.querySelector('.player-score p')
+        const computerScore = document.querySelector('.computer-score p')
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    };
+
+    const compareHands = (playerChoice, computerChoice) => {
+        //Update Text
+        const winner = document.querySelector('.winner');
+        // checking for a tie
+        if (playerChoice === computerChoice) {
+            winner.textContent = "It's a tie";
+            return;
+        }
+        // Check for rock
+        if (playerChoice === 'rock') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = "Player Wins";
+                pScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = "Computer Wins";
+                cScore++;
+                updateScore();
+                return;
+            }
+
+        }
+        //Check for paper
+        if (playerChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = "Computer Wins";
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = "Player Wins";
+                pScore++;
+                updateScore();
+                return;
+            }
+
+        }
+        // Check for scissors
+        if (playerChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                winner.textContent = "Computer Wins";
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = "Player Wins";
+                pScore++;
+                updateScore();
+                return;
+            }
+
+        }
+
+
+    };
+
+
+    //call all inner functions
+    startGame();
+    playMatch();
+
+
+};
+//start the game
+
+game();
